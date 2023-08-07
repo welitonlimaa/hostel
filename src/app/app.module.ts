d7d494de0f89c35aea877018dd392da0388076c8
welitonlimaa <welitonlima159@gmail.com>
2023-08-07 19:23:55 -0300

    app material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReservaListComponent } from './reserva-list/reserva-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,12 @@ import { ReservaListComponent } from './reserva-list/reserva-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
