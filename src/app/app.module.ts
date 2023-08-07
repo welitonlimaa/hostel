4d2fa9e847a515b2c4b2f2b9fe125fb103044464
welitonlimaa <welitonlima159@gmail.com>
2023-08-07 19:19:55 -0300

    wip: create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReservaListComponent } from './reserva-list/reserva-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReservaListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reserva-list/reserva-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-list',
+  templateUrl: './reserva-list.component.html',
+  styleUrls: ['./reserva-list.component.css']
+})
+export class ReservaListComponent {
+
+}
